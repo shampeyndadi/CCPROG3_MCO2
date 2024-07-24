712886c635bdce7b67ce386af2919b9870dd3ac1
Jedidiah <152191672+shampeyndadi@users.noreply.github.com>
2024-07-25 00:16:04 +0800

    Started implementing managehotel's view and controller

---
+++ src/ManageHotelController.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ManageHotelController {
+
+    private MainView mainView;
+    private ManageHotelView manageHotelView; 
+
+    public ManageHotelController(MainView mainView, ManageHotelView manageHotelView, Hotels myHotels){
+        this.mainView = mainView;
+        this.manageHotelView = manageHotelView;
+
+        this.manageHotelView.setHotelButtons(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JButton source = (JButton) e.getSource();
+                String hotelName = source.getText();
+                for (Hotel hotel : myHotels.HotelList()) {
+                    if (hotel.getHotelName().equals(hotelName)) {
+                        CustomizeHotelView customHotel = new CustomizeHotelView(hotel);
+
+                        mainView.setContentPane(customHotel);
+                        mainView.revalidate();
+                        mainView.repaint();
+
+                        System.out.println("You clicked on " + hotelName);
+
+                    }
+                }
+            }
+        });
+
+        this.manageHotelView.setExitButton(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                System.out.println("Exit button pressed");
+                mainView.switchToMainPanel();
+            }
+        });
+    }
+}
